400eaa0d9ee20941466d87e671a72cff36c6f796
EXVLO <nadirashvilisandro888@gmail.com>
2026-03-04 19:58:45 +0700

    updated create blade for tasks. used create blade to use as an edit page also.

--- resources/views/office/task_managers/create.blade.php
+++ resources/views/office/task_managers/create.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.app')
 
-@section('title', 'Create Task')
+@section('title', isset($task) ? 'Edit Task' : 'Create Task')
 
 @section('content')
-    <h1>Create Task</h1>
+
+    <h1>{{ isset($task) ? 'Edit Task' : 'Create Task' }}</h1>
 
     <p>
         Task Manager:
         <strong>{{ $task_manager->name }}</strong>
     </p>
 
-    <form method="POST" action="{{ route('office.tasks.store', $task_manager) }}">
+    <form method="POST"
+          action="{{ isset($task)
+        ? route('office.tasks.update', [$task_manager, $task])
+        : route('office.tasks.store', $task_manager) }}">
+
         @csrf
 
+        @isset($task)
+            @method('PATCH')
+        @endisset
+
+
+        {{-- Name --}}
         <div>
             <label>
                 Name:
-                <input type="text" name="name" value="{{ old('name') }}">
+                <input type="text"
+                       name="name"
+                       value="{{ old('name', $task->name ?? '') }}">
             </label>
+
             @error('name')
             <p style="color:red;">{{ $message }}</p>
             @enderror
@@ -25,23 +39,34 @@
 
         <br>
 
+
+        {{-- Daily Target --}}
         <div>
             <label>
-                Target Value:
-                <input type="number" name="daily_target" value="{{ old('daily_target') }}">
+                Daily Target:
+                <input type="number"
+                       name="daily_target"
+                       value="{{ old('daily_target', $task->daily_target ?? '') }}">
             </label>
-            @error('target_value')
+
+            @error('daily_target')
             <p style="color:red;">{{ $message }}</p>
             @enderror
         </div>
 
         <br>
 
+
+        {{-- Unit Type --}}
         <div>
             <label>
                 Unit Type:
-                <input type="text" name="unit_type" placeholder="pages, minutes, km..." value="{{ old('unit_type') }}">
+                <input type="text"
+                       name="unit_type"
+                       placeholder="pages, minutes, km..."
+                       value="{{ old('unit_type', $task->unit_type ?? '') }}">
             </label>
+
             @error('unit_type')
             <p style="color:red;">{{ $message }}</p>
             @enderror
@@ -49,26 +74,26 @@
 
         <br>
 
-        <div>
-            <label>
-                Start Date:
-                <input type="date" name="start_date" value="{{ old('start_date') }}">
-            </label>
-            @error('start_date')
-            <p style="color:red;">{{ $message }}</p>
-            @enderror
-        </div>
 
-        <br>
-
+        {{-- Active --}}
         <div>
             <label>
                 Active:
                 <select name="is_active">
-                    <option value="1" {{ old('is_active', '1') == '1' ? 'selected' : '' }}>Yes</option>
-                    <option value="0" {{ old('is_active') == '0' ? 'selected' : '' }}>No</option>
+
+                    <option value="1"
+                        {{ old('is_active', $task->is_active ?? 1) == 1 ? 'selected' : '' }}>
+                        Yes
+                    </option>
+
+                    <option value="0"
+                        {{ old('is_active', $task->is_active ?? 1) == 0 ? 'selected' : '' }}>
+                        No
+                    </option>
+
                 </select>
             </label>
+
             @error('is_active')
             <p style="color:red;">{{ $message }}</p>
             @enderror
@@ -76,7 +101,14 @@
 
         <br>
 
-        <button type="submit">Create Task</button>
-        <a href="{{ route('office.task_managers.show', $task_manager) }}">Cancel</a>
+        <button type="submit">
+            {{ isset($task) ? 'Update Task' : 'Create Task' }}
+        </button>
+
+        <a href="{{ route('office.task_managers.show', $task_manager) }}">
+            Cancel
+        </a>
+
     </form>
+
 @endsection
