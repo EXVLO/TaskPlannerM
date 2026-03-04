e6f50b11e0e412710acdca7bf8dbdd8bb6c7c27f
EXVLO <nadirashvilisandro888@gmail.com>
2026-03-04 15:46:49 +0700

    modified database seeder and head.blade, for failed tests purposes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,7 +8,7 @@ use App\Models\TaskEntry;
 use App\Models\TaskManager;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
