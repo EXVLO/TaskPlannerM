23c0edb41af96457faedce603b77c12ce5feaded
EXVLO <nadirashvilisandro888@gmail.com>
2026-03-04 20:02:49 +0700

    updated routes for supporting edit/delete routing

--- routes/web.php
+++ routes/web.php
@@ -20,40 +20,59 @@ Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->nam
 Route::middleware(['auth'])->get('/appsettings', [AppSettingsController::class, 'index'])->name('appsettings');
 Route::get('/news', [NewsController::class, 'index'])->name('news');
 
+
 Route::middleware(['auth'])->prefix('office')->name('office.')->group(function () {
+
     Route::get('/', [OfficeController::class, 'index'])->name('index');
 
-    // Task managers
+    // Task Managers
     Route::get('/create', [OfficeController::class, 'create'])->name('create');
     Route::post('/', [OfficeController::class, 'store'])->name('store');
 
+    Route::get('/{task_manager}/edit', [OfficeController::class, 'edit'])->name('task_managers.edit');
+
+    Route::patch('/{task_manager}', [OfficeController::class, 'update'])->name('task_managers.update');
+
+    Route::delete('/{task_manager}', [OfficeController::class, 'destroy'])->name('task_managers.destroy');
+
     Route::get('/{task_manager}', [OfficeController::class, 'showTaskManager'])->name('task_managers.show');
 
-    // Tasks (create + store)
+
+    // Tasks
     Route::get('/{task_manager}/create', [TaskController::class, 'create'])->name('tasks.create');
     Route::post('/{task_manager}/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
-    // Task details
+    Route::get('/{task_manager}/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+
+    Route::patch('/{task_manager}/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+    Route::delete('/{task_manager}/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
     Route::get('/{task_manager}/{task}', [TaskController::class, 'show'])->name('tasks.show');
 
-    Route::get(
-        '/{task_manager}/{task}/tags/{tag}',
-        [TagController::class, 'show']
-    )->name('tags.show');
 
-    Route::post(
-        '/{task_manager}/{task}/tags',
-        [TagController::class, 'store']
-    )->name('tags.store');
+    // Tags
+    Route::get('/{task_manager}/{task}/tags/{tag}', [TagController::class, 'show'])->name('tags.show');
+
+    Route::post('/{task_manager}/{task}/tags', [TagController::class, 'store'])->name('tags.store');
+
+    Route::patch('/{task_manager}/{task}/tags/{tag}', [TagController::class, 'update'])->name('tags.update');
+
+    Route::delete('/{task_manager}/{task}/tags/{tag}', [TagController::class, 'destroy'])->name('tags.destroy');
+
 });
 
+
 Route::post('/logout', function (Request $request) {
+
     Auth::logout();
 
     $request->session()->invalidate();
     $request->session()->regenerateToken();
 
-    return redirect('/'); // or redirect('/')
+    return redirect('/');
+
 })->middleware('auth')->name('logout');
+
 
 require __DIR__.'/settings.php';
